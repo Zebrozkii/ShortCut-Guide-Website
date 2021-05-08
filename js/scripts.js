f60045e5a6e7d3b7965178e04c72f36db00149e0
James Zebroski <zebrozkii@gmail.com>
2021-05-07 23:01:13 -0700

    Project done

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+function button1() {
+    var x = document.getElementById("tab");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button2() {
+    var x = document.getElementById("shift-tab");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button3() {
+    var x = document.getElementById("ctrlg");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button4() {
+    var x = document.getElementById("ctrlend");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button5() {
+    var x = document.getElementById("cntrlhome");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button6() {
+    var x = document.getElementById("altpagedown");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button7() {
+    var x = document.getElementById("ctrlpagedown");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button8() {
+    var x = document.getElementById("altpageup");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button9() {
+    var x = document.getElementById("ctrlpageup");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function button10() {
+    var x = document.getElementById("ctrlarrow");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
